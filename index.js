0e7f602fc9e88ea9a350d2beef966afb1425cd92
+ <erol.hendrickson@gmail.com>
2022-05-17 20:50:43 -0500

    commit tree 679d836372a75375154e1c60c1328bbbb0b27996

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const value = 2001
+
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return 'This one is on me!'
+  }
+  else if (value > 400 && value < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (value > 2000 && value < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (value > 2500) {
+    return 'No can do.'
+  }
+}
+  
+const city = "Pittsburgh"
+
+function ternaryCheckCity(city){
+  let destination = (city == "NYC") ? 'Ok, sounds good.' : 'No go.';
+  return destination
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const tip = 'not as generous'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch (tip) {
+   case 'generous':
+     return 'Thank you so much.'
+     break;
+  case 'not as generous':
+    return 'Thank you.'
+    break;
+  case 'thanks for everything':
+    return 'Bye.'
+   default: 'Peace out Bro.'
+     break;
+ }
 }
